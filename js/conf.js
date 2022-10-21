eb01d4992be43857e563665f53f8cb54334a1910
Muhammad Sultan Mastoi <84229548+Muhammad-Sultan@users.noreply.github.com>
2022-10-21 23:22:31 +0500

    Add files via upload

---
+++ js/conf.js
@@ -0,0 +1,31 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "admin" && password == "asian123"){
+  alert("login successful")
+  location.replace("appointment.html")
+
+// copy from here
+}
+else if ( username == "admin1" && password == "asian1234"){
+  alert("login successful")
+  location.replace("appointment.html")
+}
+// to here
+
+// then paste it below this line
+
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
